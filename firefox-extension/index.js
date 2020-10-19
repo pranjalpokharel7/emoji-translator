2a3200083990c97489ff7767e65c2c48f6ec2cb9
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2020-10-19 19:42:47 +0545

    initial commit

--- firefox-extension/index.js
+++ firefox-extension/index.js
@@ -6,24 +6,28 @@ const emojiCollection = {
 
 const stack = {
   bottom: "$",
-  top: "$",
   emoteKey: [],
   index: 0,
+  cursorEnd: -1,
 };
 
 const emojiListener = event => {
   const character = event.key;
+
   if (character == ":"){
     if (stack.bottom == "$"){
+      // stack.cursorEnd = event.target.selectionStart;
       startIndex = event.target.value.length;
       stack.bottom = ":";
     }
-    else if (stack.bottom == ":"){
-      event.preventDefault();
+    else if (stack.bottom == ":"){ 
       stack.bottom = "$";
+
       emojiName = "";
       stack.emoteKey.forEach(item => {emojiName += item;});
+
       if (emojiCollection[emojiName]){
+        event.preventDefault();
         const prevString = event.target.value;
         const startIndex = prevString.indexOf(`:${emojiName}`);
         const newString = prevString.substring(0, startIndex) + emojiCollection[emojiName] + prevString.substring(startIndex + stack.index + 1, prevString.length);
@@ -36,19 +40,34 @@ const emojiListener = event => {
   }
 
   if (stack.bottom == ":"){
-    if ((event.keyCode >= 65 && event.keyCode <= 90) || character == "_"){
-      stack.emoteKey.push(character);
-      stack.index += 1;
-    }
-    else if (event.keyCode == 8){ // for backspace
+    if (event.keyCode == 8){ // for backspace
       stack.emoteKey.splice(stack.index-1, 1);
       stack.index = stack.index > 0 ? stack.index - 1 : 0;
     }
+    else if ((event.keyCode >= 65 && event.keyCode <= 90) || character == "_" || character == " ") {
+      stack.emoteKey.push(character);
+      stack.index += 1;
+    }
+    else {
+      return;
+    }
   }
+}
 
+const testEvent = event => {
+  console.log(event.target.textContent);
 }
 
 const arrayTextInput = document.querySelectorAll("input[type=text]");
+const arrayTextArea = document.querySelectorAll("textarea");
+
 arrayTextInput.forEach((item)=>{
   item.addEventListener("keydown", emojiListener);
 });
+
+arrayTextArea.forEach((item)=>{
+  item.addEventListener("keydown", emojiListener);
+  //console.log(item); 
+});
+
+console.log(arrayTextArea);
