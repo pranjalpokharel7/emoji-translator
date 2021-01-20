268e9293b4684c5fc9560ba4bc6dc34bb278bcc8
sandesh <sandeshpokhrel54@gmail.com>
2021-01-20 14:21:25 +0545

    search tag separated for emote and emoji favorites tab added, search not implemented for emote yet

--- popup/popup-script.js
+++ popup/popup-script.js
@@ -61,47 +61,97 @@ function filter()
 {
     let input = searchDoc.value.toLowerCase();
     let emos = document.querySelectorAll(".emoji-button");
+    let emotes = document.querySelectorAll(".emote-button");
 
-    if(input){
-        pages.forEach(item => item.style.display = "block")
+    // if (emojiTable.style.display === "flex")
+    // {
+       if(input){
+        pages.forEach(item => item.style.display = "block");
 
-        for(let i = 0; i<emos.length; i++)
-        {
-            let emoname = emos[i].id.substring(1, emos[i].id.length-1);
-
-            if(emoname.indexOf(input) > -1)
+            for(let i = 0; i<emos.length; i++)
             {
-                //do nothing
-                console.log(emoname);
-                emos[i].style.display = "inline-block";
-            }
-            else{
-                emos[i].style.display = "none";
-            }
+                let emoname = emos[i].id.substring(1, emos[i].id.length-1);
 
+                if(emoname.indexOf(input) > -1)
+                {
+                    //do nothing
+                    console.log(emoname);
+                    emos[i].style.display = "inline-block";
+                }
+                else{
+                    emos[i].style.display = "none";
+                }
+
+            }
         }
     }
+    
+    // else if(emoteTable.style.display === "flex")
+    // {
+        // if(input){
+        //     for(let i = 0; i<emotes.length; i++)
+        //     {
+        //         let emotename = emotes[i].id.substring(1, emotes[i].id.length-1);
 
+        //         if(emotename.indexOf(input) > -1)
+        //         {
+        //             //do nothing
+        //             console.log(emotename);
+        //             emotes[i].style.display = "inline-block";
+        //         }
+        //         else{
+        //             emotes[i].style.display = "none";
+        //         }
+
+        //     }
+            
+        // }
+        //search in the emote area
+    // }
+    //bug if entered and cleared doesn't show initial set of emojis 
     //back to normal aka is shuffled once search is used
-}
+// }
 
 let searchDoc = document.querySelector(".search");
 searchDoc.addEventListener("keyup", filter);
 
 //buttons
+let previousBtn = document.querySelector("#previous");
+let nextBtn = document.querySelector("#next");
 let emojiToggle = document.querySelector("#emoji-toggle");
 let emoteToggle = document.querySelector("#emote-toggle");
+let favoritesButton = document.querySelector("#favorites");
 
 //tables
 let emojiTable = document.querySelector("#emoji-table");
 let emoteTable = document.querySelector("#emoticon-table");
+let favoritesTable = document.querySelector("#fab-table");
 
 emojiToggle.addEventListener("click",function(){
     emoteTable.style.display = "none";
+    favoritesTable.style.display = "none";
     emojiTable.style.display = "flex";
+    searchDoc.style.display = "inline";
+    previousBtn.style.display = "inline";
+    nextBtn.style.display = "inline";
+    
 });
 
 emoteToggle.addEventListener("click",function(){
     emojiTable.style.display = "none";
+    favoritesTable.style.display = "none";
     emoteTable.style.display = "flex";
+    previousBtn.style.display = "none";
+    nextBtn.style.display = "none";
+    searchDoc.style.display = "inline";
 });
+
+favoritesButton.addEventListener("click",function(){
+    emojiTable.style.display = "none";
+    emoteTable.style.display = "none";
+    favoritesTable.style.display = "flex";
+    searchDoc.style.display = "none";
+    previousBtn.style.display = "none";
+    nextBtn.style.display = "none";
+});
+
