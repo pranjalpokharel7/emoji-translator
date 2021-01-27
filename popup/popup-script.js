17c33e02804871035036e3c0e52dea693bb7a24e
sandesh <sandeshpokhrel54@gmail.com>
2021-01-27 22:13:41 +0545

    new tabs and search fix

--- popup/popup-script.js
+++ popup/popup-script.js
@@ -63,54 +63,85 @@ function filter()
     let emos = document.querySelectorAll(".emoji-button");
     let emotes = document.querySelectorAll(".emote-button");
 
-    // if (emojiTable.style.display === "flex")
-    // {
-       if(input){
-        pages.forEach(item => item.style.display = "block");
 
-            for(let i = 0; i<emos.length; i++)
+    if(emojiTable.style.display === "flex")
+    {
+
+            if(input === '')
             {
-                let emoname = emos[i].id.substring(1, emos[i].id.length-1);
-
-                if(emoname.indexOf(input) > -1)
-                {
-                    //do nothing
-                    console.log(emoname);
-                    emos[i].style.display = "inline-block";
-                }
-                else{
-                    emos[i].style.display = "none";
-                }
-
+                emojiStyleReset();
             }
-        }
+
+            else{
+                pages.forEach(item => item.style.display = "block");
+
+                for(let i = 0; i<emos.length; i++)
+                {
+                    let emoname = emos[i].id.substring(1, emos[i].id.length-1);
+
+                    if(emoname.indexOf(input) > -1) //returns first index at which the search val is found
+                    {
+                        //do nothing
+                        // console.log(emoname);
+                        emos[i].style.display = "inline-block";
+                    }
+                    else{
+                        emos[i].style.display = "none";
+                    }
+
+                }
+            }
     }
-    
-    // else if(emoteTable.style.display === "flex")
-    // {
-        // if(input){
-        //     for(let i = 0; i<emotes.length; i++)
-        //     {
-        //         let emotename = emotes[i].id.substring(1, emotes[i].id.length-1);
 
-        //         if(emotename.indexOf(input) > -1)
-        //         {
-        //             //do nothing
-        //             console.log(emotename);
-        //             emotes[i].style.display = "inline-block";
-        //         }
-        //         else{
-        //             emotes[i].style.display = "none";
-        //         }
 
-        //     }
-            
-        // }
-        //search in the emote area
+    // if (emotes.style.display === "flex"){
+        
+    //     //emotes
+    //     if(input === '')
+    //     {
+    //         emoteStyleReset();
+    //     }
+
+    //     else{
+
+    //         for(let i = 0; i<emotes.length ; i++)
+    //         {
+    //             let emotename = emotes[i].id.substring(1, emotes[i].id.length-1);
+
+    //             if(emotename.indexOf(input) > -1)
+    //             {
+    //                 // console.log(emotename);
+    //                 emotes[i].style.display = "inline-block";
+    //             }
+
+    //             else{
+    //                 emotes[i].style.display = "none";
+    //             }
+    //         }
+    //     }
     // }
-    //bug if entered and cleared doesn't show initial set of emojis 
-    //back to normal aka is shuffled once search is used
-// }
+}
+
+function emojiStyleReset()
+{
+    let emos = document.querySelectorAll(".emoji-button");
+
+    for(let i=0; i<emos.length; i++)
+    {
+        emos[i].style.display = "inline-block";
+    }
+}
+
+function emoteStyleReset()
+{
+    let emotes = document.querySelectorAll(".emote-button");
+
+    for(let i=0; i<emotes.length; i++)
+    {
+        emotes[i].style.display = "inline-block";
+    }
+}
+
 
 let searchDoc = document.querySelector(".search");
 searchDoc.addEventListener("keyup", filter);
@@ -121,37 +152,65 @@ let nextBtn = document.querySelector("#next");
 let emojiToggle = document.querySelector("#emoji-toggle");
 let emoteToggle = document.querySelector("#emote-toggle");
 let favoritesButton = document.querySelector("#favorites");
+let recentsButton = document.querySelector("#recents");
 
 //tables
 let emojiTable = document.querySelector("#emoji-table");
 let emoteTable = document.querySelector("#emoticon-table");
 let favoritesTable = document.querySelector("#fab-table");
+let recentsTable = document.querySelector("#recents-table");
 
 emojiToggle.addEventListener("click",function(){
+
     emoteTable.style.display = "none";
     favoritesTable.style.display = "none";
+    recentsTable.style.display = "none";
     emojiTable.style.display = "flex";
-    searchDoc.style.display = "inline";
-    previousBtn.style.display = "inline";
+    searchDoc.style.display = "inline";  //search option
+    previousBtn.style.display = "inline";   //prev and next button option
     nextBtn.style.display = "inline";
-    
+    searchDoc.value = '';
+    emojiStyleReset();
+ 
 });
 
 emoteToggle.addEventListener("click",function(){
     emojiTable.style.display = "none";
     favoritesTable.style.display = "none";
+    recentsTable.style.display = "none";
     emoteTable.style.display = "flex";
     previousBtn.style.display = "none";
     nextBtn.style.display = "none";
-    searchDoc.style.display = "inline";
+    searchDoc.style.display = "inline"; //search option
+    searchDoc.value = '';
+    emoteStyleReset();
+
 });
 
 favoritesButton.addEventListener("click",function(){
     emojiTable.style.display = "none";
     emoteTable.style.display = "none";
+    recentsTable.style.display = "none";
     favoritesTable.style.display = "flex";
     searchDoc.style.display = "none";
     previousBtn.style.display = "none";
     nextBtn.style.display = "none";
 });
 
+recentsButton.addEventListener("click",function(){
+
+    emojiTable.style.display = "none";
+    emoteTable.style.display = "none";
+    favoritesTable.style.display = "none";
+    recentsTable.style.display = "flex";
+    searchDoc.style.display = "none";
+    previousBtn.style.display = "none";
+    nextBtn.style.display = "none";
+});
+
+
+//TODO
+//replace buttons with icons preferably color changable icons to say where user is at
+//if searchdoc. value == 0 whether then display normal
+// else if something is entered trigger filter
+//check where the search is being done emoji table or emote table before filtering
