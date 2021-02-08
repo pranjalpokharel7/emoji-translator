079e37e75933fbb3a044628b897bd3e0f631dc05
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2021-02-09 03:46:56 +0545

    search as a separate entity

--- popup/popup-script.js
+++ popup/popup-script.js
@@ -66,94 +66,40 @@ const pageScroll = (event) => {
 next.addEventListener("click", pageScroll); 
 previous.addEventListener("click", pageScroll); 
 
+const searchPage = document.querySelector('#search-results-page');
+const tabPages = document.querySelectorAll('.tab-page');
+const allEmojis = Array.from(document.querySelectorAll(".emoji-button"));
 
-function filter()
+function filterEmojis()
 {
     let input = searchDoc.value.toLowerCase();
-    let emos = document.querySelectorAll(".emoji-button");
-    let emotes = document.querySelectorAll(".emote-button");
-
-    if(emojiTable.style.display === "flex")
-    {
-
-        if(input === '')
-        {
-            emojiStyleReset();
-        }
-
-        else{
-            pages.forEach(item => item.style.display = "block");
-
-            for(let i = 0; i<emos.length; i++)
-            {
-                let emoname = emos[i].id.substring(1, emos[i].id.length-1);
-
-                if(emoname.indexOf(input) > -1) //returns first index at which the search val is found
-                {
-                    //do nothing
-                    // console.log(emoname);
-                    emos[i].style.display = "inline-block";
-                }
-                else{
-                    emos[i].style.display = "none";
-                }
-
-            }
-        }
+    if (input == ""){
+        emojiStyleReset();
+        searchPage.style.display = "none";
+        recentsTable.style.display = "flex";
+        return;
     }
 
+    tabPages.forEach(tabPage => tabPage.style.display = "none");
+    searchPage.style.display = "flex";
+    let searchResults = new Set();
 
-    // if (emotes.style.display === "flex"){
-
-    //     //emotes
-    //     if(input === '')
-    //     {
-    //         emoteStyleReset();
-    //     }
-
-    //     else{
-
-    //         for(let i = 0; i<emotes.length ; i++)
-    //         {
-    //             let emotename = emotes[i].id.substring(1, emotes[i].id.length-1);
-
-    //             if(emotename.indexOf(input) > -1)
-    //             {
-    //                 // console.log(emotename);
-    //                 emotes[i].style.display = "inline-block";
-    //             }
-
-    //             else{
-    //                 emotes[i].style.display = "none";
-    //             }
-    //         }
-    //     }
-    // }
+    allEmojis.forEach(emoji => {
+        let emojiName = emoji.id.substring(1, emoji.id.length);
+        if (emojiName.indexOf(input) > -1)
+            searchResults.add(emoji);
+    });
+    emojiStyleReset();
+    searchResults.forEach(buttonNode => searchPage.append(buttonNode));
 }
 
 function emojiStyleReset()
 {
-    let emos = document.querySelectorAll(".emoji-button");
-
-    for(let i=0; i<emos.length; i++)
-    {
-        emos[i].style.display = "inline-block";
-    }
+    searchPage.innerHTML = "";
 }
 
-function emoteStyleReset()
-{
-    let emotes = document.querySelectorAll(".emote-button");
-
-    for(let i=0; i<emotes.length; i++)
-    {
-        emotes[i].style.display = "inline-block";
-    }
-}
-
-
-let searchDoc = document.querySelector(".search");
-searchDoc.addEventListener("keyup", filter);
+let searchDoc = document.querySelector("#search");
+searchDoc.addEventListener("keyup", filterEmojis);
 
 // the toggle buttons code are redundant, refactoring needed
 
@@ -172,17 +118,10 @@ let favoritesTable = document.querySelector("#fab-table");
 let recentsTable = document.querySelector("#recents-table");
 
 emojiToggle.addEventListener("click",function(){
-
     emoteTable.style.display = "none";
     favoritesTable.style.display = "none";
     recentsTable.style.display = "none";
     emojiTable.style.display = "flex";
-    searchDoc.style.display = "inline";  //search option
-    previousBtn.style.display = "inline";   //prev and next button option
-    nextBtn.style.display = "inline";
-    searchDoc.value = '';
-    emojiStyleReset();
-
 });
 
 emoteToggle.addEventListener("click",function(){
@@ -190,12 +129,6 @@ emoteToggle.addEventListener("click",function(){
     favoritesTable.style.display = "none";
     recentsTable.style.display = "none";
     emoteTable.style.display = "flex";
-    previousBtn.style.display = "none";
-    nextBtn.style.display = "none";
-    searchDoc.style.display = "inline"; //search option
-    searchDoc.value = '';
-    emoteStyleReset();
-
 });
 
 favoritesButton.addEventListener("click",function(){
@@ -203,20 +136,13 @@ favoritesButton.addEventListener("click",function(){
     emoteTable.style.display = "none";
     recentsTable.style.display = "none";
     favoritesTable.style.display = "flex";
-    searchDoc.style.display = "none";
-    previousBtn.style.display = "none";
-    nextBtn.style.display = "none";
 });
 
 recentsButton.addEventListener("click",function(){
-
     emojiTable.style.display = "none";
     emoteTable.style.display = "none";
     favoritesTable.style.display = "none";
     recentsTable.style.display = "flex";
-    searchDoc.style.display = "none";
-    previousBtn.style.display = "none";
-    nextBtn.style.display = "none";
 });
 
 
